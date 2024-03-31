2671f08ceb184a0eb800327743222a5233ab4109
prerna-codess <prerna.codess@gmail.com>
2024-03-31 13:40:06 +0530

    Made a separate file for constants

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import {useDispatch} from "react-redux";
 import {addUser, removeUser} from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () =>{
     const navigate = useNavigate();
@@ -46,13 +47,13 @@ const Header = () =>{
             <img 
                 className="w-44"
                 alt="app-logo" 
-                src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"/>
+                src = {LOGO}/>
         
             {user && <div className=" flex p-2">
                 <img
                     className="w-12 h-12 "
                     alt="user-icon" 
-                    src={user.photoURL}/>
+                    src={user?.photoURL}/>
                 <button 
                     onClick={handleSignOut}
                     className="font=bold text-white">Sign Out</button>
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import {checkValidity} from "../utils/validate";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm]= useState(true);
@@ -39,7 +40,7 @@ const Login = () => {
                 // Signed up 
                 const user = userCredential.user;
                 updateProfile(user, {
-                    displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/133656308?v=4"
+                    displayName: name.current.value, photoURL: USER_AVATAR,
                   }).then(() => {
                     // Profile updated!
                     const {uid, email, displayName, photoURL} = auth.currentUser;
