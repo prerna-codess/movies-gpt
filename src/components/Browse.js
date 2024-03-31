19964b2c50791811af3041a2e9f2ebc466b4a61c
prerna-codess <prerna.codess@gmail.com>
2024-03-31 18:57:46 +0530

    TMDB API data fecth and created main container

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,5 +1,7 @@
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
     useNowPlayingMovies();
@@ -7,6 +9,18 @@ const Browse = () => {
     return(
         <div>
             <Header/>
+            <MainContainer/>
+            <SecondaryContainer/>
+
+            {/**
+             - main container
+               -video background
+               -video title
+            - secondary container
+              - movie List*n
+                - cards*n
+             */}
+    
         </div>
     )
 }
