2ccf639911e952ac1c0ce17a8869d1b7f4205aa3
prerna-codess <prerna.codess@gmail.com>
2024-04-11 15:43:39 +0530

    Hosted project

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+//Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqiszeeNp45GEcITMkQysxb7OvRmE5GJg",
-  authDomain: "movies-gpt-2a4e6.firebaseapp.com",
-  projectId: "movies-gpt-2a4e6",
-  storageBucket: "movies-gpt-2a4e6.appspot.com",
-  messagingSenderId: "920330217229",
-  appId: "1:920330217229:web:bdbb3362b77d2e09967244",
-  measurementId: "G-5TPZPLXRLZ"
+  apiKey: "AIzaSyDKE4o9bXl3_P4NkQp5Kbn0af-qNfYhB4Q",
+  authDomain: "my-movies-gpt-b5303.firebaseapp.com",
+  projectId: "my-movies-gpt-b5303",
+  storageBucket: "my-movies-gpt-b5303.appspot.com",
+  messagingSenderId: "552618892892",
+  appId: "1:552618892892:web:a3a319fafb7b4b29a19099",
+  measurementId: "G-3CQEEGREXK"
 };
 
 // Initialize Firebase
